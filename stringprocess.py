702632a46d61d8c4ac346663fd5d599b30945e87
iamkaushal <kaushalsharma880@gmail.com>
2018-11-17 22:17:34 +0530

    log file processing done

--- stringprocess.py
+++ stringprocess.py
@@ -16,9 +16,20 @@ for error in error_matches:
 
 
 
+# Str pattern analyser
+str_pattern = re.compile(r'str\d[\w -?]*')
+str_matches = str_pattern.findall(file_content)
 
 
 
+working_pattern = re.compile(r'Working..: (\w*)')
+working = working_pattern.findall(file_content)[0]
+
+GoneBit = re.findall(r'GoneBit..: (\w*)', file_content)[0]
+
+Errors = re.findall(r'Errors..: (\w*)', file_content)[0]
+
+Warnings = re.findall(r'Warnings: (\w*)', file_content)[0]
 
 
 
@@ -70,51 +81,3 @@ first_program_version   = details_pattern.findall(temp[13])[0][1]
 #     print(details_pattern.findall(temp[i])[0][2])
 
 # errors_list = re.search("(\*ERROR\*\,)([\w ]+)", s)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#
-#discarded code
-#
-# log_array = re.split("\s", s)
-#
-# for i in range(len(log_array)):
-#     print( str(i) + " " + log_array[i])
-#
-#
-# agent_version = log_array[4]
-# dlp_family = log_array[8] + " " + log_array[9]
-# system_number = log_array[13] + " " + log_array[14] + " " + log_array[15]
-# srx_date = log_array[19] + " " + log_array[20]
-# figr_file_used = log_array[25]
-# cache_type = log_array[29]
-# child_process_type = log_array[33]
-# IServer_address = log_array[37]
-# IServer_version = log_array[41] + " " + log_array[42] + " " + log_array[43] + " " + log_array[44] + " " + log_array[45]
-# Crnt_mmm_mthd = log_array[50] + " " + log_array[51]
-# date_of_logfile = log_array[56] + " "  + log_array[57] + " "  + log_array[58] + " "  + log_array[59] + " "  + log_array[60]
-# cache_slss = log_array[64] + " "  + log_array[65] + " "  + log_array[66] + " "  + log_array[67] + " "  + log_array[68]
-# first_program_version =  log_array[72] + " " + log_array[73] + " " + log_array[74] + " "  + log_array[75] + " "   + log_array[76] + " "  + log_array[77] + " "  + log_array[78]
-#
-#
-# warnings = log_array[124]
-# errors = log_array[122]
-# GoneBit = log_array[120]
-# Working = log_array[118]
