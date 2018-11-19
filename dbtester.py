ecd9e7a8cb7c155ebbfde3d3bb8a881f7f483c0a
iamkaushal <kaushalsharma880@gmail.com>
2018-11-20 01:09:57 +0530

    update database insertion

--- dbtester.py
+++ dbtester.py
@@ -16,122 +16,69 @@ cur.execute("SELECT * FROM examples")
 # print the first and second columns
 for row in cur.fetchall() :
     print (row[0], " ", row[1])
-#
-# import json
-#
-# v = [1, 2, 4]
-# s = json.dumps(v)
 
+create database `dbtester`;
 
-#
-# INSERT into log (agent_ver ,dlp_fam ,sys_num ,srx_date ,
-# figr_fileused ,cache_type ,child_process_type ,
-# iserver_add ,iserver_ver ,crnt_mmm_mthd ,date_of_logfile ,
-# cache_slss ,first_prog_ver ,errors_json ,str_json ,working ,gonebit ,error ,warning )
-#
-# VALUES (agent_version, dlp_family, system_number, srx_date, figr_file_used, cache_type, child_process_type,
-#  IServer_address, IServer_version, Crnt_mmm_mthd, date_of_logfile, cache_slss, first_program_version
-# errors_json, str_json, working, GoneBit, Errors, Warnings );
+CREATE TABLE `dbtester`.`users` (
+  `id` INT NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NULL,
+  `creation_date` DATETIME NULL,
+  PRIMARY KEY (`id`, `username`)
+);
 
-
-#
-# create database `dbtester`;
-# CREATE  TABLE `dbtester`.`users` (
-#
-#   `id` INT NOT NULL ,
-#
-#   `username` VARCHAR(45) NOT NULL ,
-#
-#   `password` VARCHAR(45) NULL ,
-#
-#   `creation_date` DATETIME NULL ,
-#
-#   PRIMARY KEY (`id`, `username`) );
-#
-# CREATE  TABLE `dbtester`.`log_details` (
-#
-#   `id` BIGINT NOT NULL ,
-#
-#   `file_path` VARCHAR(500) NULL ,
-#
-#   `creation_date` DATETIME NULL ,
-#
-#   `insert_date` DATETIME NULL ,
-#
-#   `agent_version` VARCHAR(100) NULL ,
-#
-#   `dlp_family` VARCHAR(200) NULL ,
-#
-#   `system_number` VARCHAR(200) NULL ,
-#
-#   `srx_date` VARCHAR(200) NULL ,
-#
-#   `figr` VARCHAR(200) NULL ,
-#
-#   `cache_type` VARCHAR(200) NULL ,
-#
-#   `process_type` VARCHAR(100) NULL ,
-#
-#   `iserver_address` VARCHAR(150) NULL ,
-#
-#   `iserver_version` VARCHAR(200) NULL ,
-#
-#   `mmm_method` VARCHAR(150) NULL ,
-#
-#   `log_date` DATETIME NULL ,
-#
-#   `cache_slss` VARCHAR(150) NULL ,
-#
-#   `program_version` VARCHAR(150) NULL ,
-#
-#   `working` INT NULL ,
-#
-#   `gone_bit` INT NULL ,
-#
-#   `errors` INT NULL ,
-#
-#   `warnings` INT NULL ,
-#
-#   `file_name` VARCHAR(100) NULL ,
-#
-#   PRIMARY KEY (`id`) );
-#
-
-
-# create table log (
-#   id int NOT NULL AUTO_INCREMENT,
-#   PRIMARY KEY (id),
-#   agent_ver varchar(128),
-#   dlp_fam varchar(128),
-#   sys_num varchar(128),
-#   srx_date varchar(128),
-#   figr_fileused varchar(128),
-#   cache_type varchar(128),
-#   child_process_type varchar(128),
-#   iserver_add varchar(128),
-#   iserver_ver varchar(128),
-#   crnt_mmm_mthd varchar(128),
-#   date_of_logfile varchar(128),
-#   cache_slss varchar(256),
-#   first_prog_ver varchar(256),
-#   errors_json varchar(500),
-#   str_json varchar(500),
-#   working varchar(10),
-#   gonebit varchar(10),
-#   error varchar(10),
-#   warning varchar(10)
-# );
-#
-
-#
-
-# CREATE TABLE IF NOT EXISTS examples (
-#   id int(11) NOT NULL AUTO_INCREMENT,
-#   description varchar(45),
-#   PRIMARY KEY (id)
+# CREATE TABLE `dbtester`.`log_details` (
+#   `id` BIGINT NOT NULL,
+#   `file_path` VARCHAR(500) NULL,
+#   `creation_date` DATETIME NULL,
+#   `insert_date` DATETIME NULL,
+#   `agent_version` VARCHAR(100) NULL,
+#   `dlp_family` VARCHAR(200) NULL,
+#   `system_number` VARCHAR(200) NULL,
+#   `srx_date` VARCHAR(200) NULL,
+#   `figr` VARCHAR(200) NULL,
+#   `cache_type` VARCHAR(200) NULL,
+#   `process_type` VARCHAR(100) NULL,
+#   `iserver_address` VARCHAR(150) NULL,
+#   `iserver_version` VARCHAR(200) NULL,
+#   `mmm_method` VARCHAR(150) NULL,
+#   # `log_date` DATETIME NULL,
+#   `log_date` VARCHAR(150) NULL,
+#   `cache_slss` VARCHAR(150) NULL,
+#   `program_version` VARCHAR(150) NULL,
+#   `working` INT NULL,
+#   `gone_bit` INT NULL,
+#   `errors` INT NULL,
+#   `warnings` INT NULL,
+#   `file_name` VARCHAR(100) NULL,
+#   PRIMARY KEY (`id`)
 # );
 
 
-# INSERT INTO examples(description) VALUES ("Hello World");
-# INSERT INTO examples(description) VALUES ("MySQL Example");
-# INSERT INTO examples(description) VALUES ("Flask Example");
+create table log_details (
+  id int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  file_path VARCHAR(500) NULL,
+  creation_date DATETIME NULL,
+  insert_date DATETIME NULL,
+  file_name VARCHAR(100) NULL,
+  agent_ver varchar(128),
+  dlp_fam varchar(128),
+  sys_num varchar(128),
+  srx_date varchar(128),
+  figr_fileused varchar(128),
+  cache_type varchar(128),
+  child_process_type varchar(128),
+  iserver_add varchar(128),
+  iserver_ver varchar(128),
+  crnt_mmm_mthd varchar(128),
+  date_of_logfile varchar(128),
+  cache_slss varchar(256),
+  first_prog_ver varchar(256),
+  errors_json varchar(500),
+  str_json varchar(500),
+  working varchar(10),
+  gonebit varchar(10),
+  error varchar(10),
+  warning varchar(10)
+);
--- stringprocess.py
+++ stringprocess.py
@@ -1,16 +1,14 @@
 import re
+import json
 
 with open('Drlll_cfg_D40_11_252_112_121_0_2018-September-19_2148b.err', 'r' ) as f:
 # with open('Srlll_cfg_K40_11_252_112_122_0_2018-September-20_2148b.err', 'r' ) as f:
     file_content = f.read()
 
-
 # ERROR Pattern analyser
 error_pattern = re.compile(r'(\*ERROR\*\,)([\w ]+)')
 error_matches = error_pattern.findall(file_content)
 # subbed_errors = pattern.sub(r'\1', s)
-
-import json
 
 errors_json = str(json.dumps(error_matches))
 
@@ -36,19 +34,19 @@ details_pattern = re.compile(r'(:\s)(.*)')
 # temp variable to store the details_pattern found in file_content
 temp = re.split('\n', file_content)
 
-agent_version           = details_pattern.findall(temp[1])[0][1]
-dlp_family              = details_pattern.findall(temp[2])[0][1]
-system_number           = details_pattern.findall(temp[3])[0][1]
-srx_date                = details_pattern.findall(temp[4])[0][1]
-figr_file_used          = details_pattern.findall(temp[5])[0][1]
-cache_type              = details_pattern.findall(temp[6])[0][1]
-child_process_type      = details_pattern.findall(temp[7])[0][1]
-IServer_address         = details_pattern.findall(temp[8])[0][1]
-IServer_version         = details_pattern.findall(temp[9])[0][1]
-Crnt_mmm_mthd           = details_pattern.findall(temp[10])[0][1]
-date_of_logfile         = details_pattern.findall(temp[11])[0][1]
-cache_slss              = details_pattern.findall(temp[12])[0][1]
-first_program_version   = details_pattern.findall(temp[13])[0][1]
+agent_version           = details_pattern.findall(temp[1])[0][1].rstrip()
+dlp_family              = details_pattern.findall(temp[2])[0][1].rstrip()
+system_number           = details_pattern.findall(temp[3])[0][1].rstrip()
+srx_date                = details_pattern.findall(temp[4])[0][1].rstrip()
+figr_file_used          = details_pattern.findall(temp[5])[0][1].rstrip()
+cache_type              = details_pattern.findall(temp[6])[0][1].rstrip()
+child_process_type      = details_pattern.findall(temp[7])[0][1].rstrip()
+IServer_address         = details_pattern.findall(temp[8])[0][1].rstrip()
+IServer_version         = details_pattern.findall(temp[9])[0][1].rstrip()
+Crnt_mmm_mthd           = details_pattern.findall(temp[10])[0][1].rstrip()
+date_of_logfile         = details_pattern.findall(temp[11])[0][1].rstrip()
+cache_slss              = details_pattern.findall(temp[12])[0][1].rstrip()
+first_program_version   = details_pattern.findall(temp[13])[0][1].rstrip()
 
 
 import MySQLdb
@@ -60,26 +58,29 @@ db = MySQLdb.connect(host="localhost",  # your host
                      db="dbtester")   # name of the database
 
 # Create a Cursor object to execute queries.
-
-# Create a Cursor object to execute queries.
 cur = db.cursor()
 #
 
-query = 'INSERT into log (id , agent_ver ,dlp_fam ,sys_num ,srx_date , figr_fileused ,cache_type ,child_process_type ,iserver_add ,iserver_ver ,crnt_mmm_mthd ,date_of_logfile ,cache_slss ,first_prog_ver ,errors_json ,str_json ,working ,gonebit ,error ,warning ) VALUES (1, 'agent_version', dlp_family, system_number, srx_date, figr_file_used, cache_type, child_process_type, IServer_address, IServer_version, Crnt_mmm_mthd, date_of_logfile, cache_slss, first_program_version, errors_json, str_json, working, GoneBit, Errors, Warnings );'
+query = '''INSERT into log_details ( agent_ver ,dlp_fam ,sys_num ,srx_date , figr_fileused ,
+cache_type ,child_process_type ,iserver_add ,iserver_ver ,crnt_mmm_mthd ,date_of_logfile ,
+cache_slss ,first_prog_ver ,errors_json ,str_json ,working ,gonebit ,error ,warning )
 
-# values( "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello");
+VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
 
-# query = select * from log;
-# Select data from table using SQL query.
-cur.execute(query)
+data = [agent_version, dlp_family, system_number, srx_date, figr_file_used,
+ cache_type, child_process_type, IServer_address, IServer_version, Crnt_mmm_mthd,
+ date_of_logfile, cache_slss, first_program_version, errors_json, str_json, working,
+  GoneBit, Errors, Warnings ]
 
+# insert data into table using SQL query.
+cur.execute(query, data)
 db.commit()
 
 
 # DEBUGGING CODE
 # for i in range(1,14):
 #     print(i, (details_pattern.findall(temp[i])[0][1]))
-
+#
 # print(agent_version)
 # print(dlp_family)
 # print(system_number)
