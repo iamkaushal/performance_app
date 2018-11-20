0d7b483e1375ab4782d42b8d249f20962342529c
iamkaushal <kaushalsharma880@gmail.com>
2018-11-20 13:06:53 +0530

    updated db settings

--- dbtester.py
+++ dbtester.py
@@ -27,6 +27,36 @@ CREATE TABLE `dbtester`.`users` (
   PRIMARY KEY (`id`, `username`)
 );
 
+
+create table log_details (
+id int NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (id),
+file_path VARCHAR(500) NULL,
+creation_date DATETIME NULL,
+insert_date DATETIME NULL,
+file_name VARCHAR(100) NULL,
+agent_ver varchar(128),
+dlp_fam varchar(128),
+sys_num varchar(128),
+srx_date varchar(128),
+figr_fileused varchar(128),
+cache_type varchar(128),
+child_process_type varchar(128),
+iserver_add varchar(128),
+iserver_ver varchar(128),
+crnt_mmm_mthd varchar(128),
+date_of_logfile varchar(128),
+cache_slss varchar(256),
+first_prog_ver varchar(256),
+errors_json varchar(500),
+str_json varchar(500),
+working varchar(10),
+gonebit varchar(10),
+error varchar(10),
+warning varchar(10)
+);
+
+
 # CREATE TABLE `dbtester`.`log_details` (
 #   `id` BIGINT NOT NULL,
 #   `file_path` VARCHAR(500) NULL,
@@ -53,32 +83,3 @@ CREATE TABLE `dbtester`.`users` (
 #   `file_name` VARCHAR(100) NULL,
 #   PRIMARY KEY (`id`)
 # );
-
-
-create table log_details (
-  id int NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id),
-  file_path VARCHAR(500) NULL,
-  creation_date DATETIME NULL,
-  insert_date DATETIME NULL,
-  file_name VARCHAR(100) NULL,
-  agent_ver varchar(128),
-  dlp_fam varchar(128),
-  sys_num varchar(128),
-  srx_date varchar(128),
-  figr_fileused varchar(128),
-  cache_type varchar(128),
-  child_process_type varchar(128),
-  iserver_add varchar(128),
-  iserver_ver varchar(128),
-  crnt_mmm_mthd varchar(128),
-  date_of_logfile varchar(128),
-  cache_slss varchar(256),
-  first_prog_ver varchar(256),
-  errors_json varchar(500),
-  str_json varchar(500),
-  working varchar(10),
-  gonebit varchar(10),
-  error varchar(10),
-  warning varchar(10)
-);
