edf7cf40b9814f98df3f3b14eba03162654ab552
iamkaushal <kaushalsharma880@gmail.com>
2018-11-20 13:06:30 +0530

    updated dashboard

--- PerformanceMonitor/login.py
+++ PerformanceMonitor/login.py
@@ -2,16 +2,16 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen , SlideTransition
 from kivy.lang.builder import Builder
 from kivy.properties import StringProperty
-import MySQLdb
+from database import DatabaseManagement
 
-db = MySQLdb.connect(host="localhost",user="root", passwd="",db="dbtester")
 Builder.load_file('login.kv')
 class LoginWindow(Screen):
     def login(self,usernameText,passwordText):
+        db = DatabaseManagement()
         app = App.get_running_app()
         app.username = usernameText
         app.password = passwordText
-        authenticationResult = self.authenticate(usernameText,passwordText)
+        authenticationResult = db.authenticate(usernameText,passwordText)
         if authenticationResult == "True":
             self.manager.transition = SlideTransition(direction="left")
             self.manager.current = "Dashboard"
@@ -20,17 +20,3 @@ class LoginWindow(Screen):
         
     def build(self):
         pass
-
-    def authenticate(self,username,password):
-        cursor = db.cursor()
-        cursor.execute("select username from users where username = %s",[username])
-        dbuser = cursor.fetchone()[0]
-        if dbuser == username:
-            cursor.execute("select password from users where username = %s",[username])
-            dbpass = cursor.fetchone()[0]
-            if dbpass == password:
-                return "True"
-            else:
-                return "Password Incorrect"
-        else:
-            return "Username Incorrect."
--- PerformanceMonitor/main.py
+++ PerformanceMonitor/main.py
@@ -5,14 +5,17 @@ from kivy.properties import StringProperty
 from login import LoginWindow
 from dashboard import DashboardWindow
 from kivy.core.window import Window
+from fileprocessor import FileProcessor
 Window.size = (1500, 700)
 class PerformanceMonitorApp(App):
     username = StringProperty(None)
     password = StringProperty(None)
     def build(self):
+        f = FileProcessor()
         manager = ScreenManager()
         manager.add_widget(LoginWindow(name="Login"))
         manager.add_widget(DashboardWindow(name="Dashboard"))
+        f.processFiles()
         return manager
 
 if __name__ == '__main__':
